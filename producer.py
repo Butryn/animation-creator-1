abfd38c6132511ff0b31cb8a3f994e9e6295cc7e
Butryn <r.butryn@op.pl>
2018-06-07 15:16:25 +0000

    dodanie producer.py

---
+++ producer.py
@@ -0,0 +1,7 @@
+import json, boto3, uuid
+
+sqs = boto3.resource('sqs', region_name="eu-central-1")
+
+tweets = sqs.get_queue_by_name(QueueName='s179849')
+response = tweets.send_message(MessageBody='Hello World'
+)
