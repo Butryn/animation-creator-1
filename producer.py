021c09199d3ab204276e8c44cbd561474e609eb8
Butryn <r.butryn@op.pl>
2018-06-12 17:35:49 +0000

    Poprawa producer.py

--- producer.py
+++ producer.py
@@ -1,7 +1,6 @@
-import json, boto3, uuid
+import json, boto3, uuid, sys
 
 sqs = boto3.resource('sqs', region_name="eu-central-1")
 
 tweets = sqs.get_queue_by_name(QueueName='s179849')
-response = tweets.send_message(MessageBody='Hello World'
-)
+response = tweets.send_message(MessageBody=sys.argv[1])
